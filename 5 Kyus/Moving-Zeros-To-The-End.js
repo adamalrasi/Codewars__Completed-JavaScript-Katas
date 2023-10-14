7397fd091c2e8396882c91de040e3fd34a67f8ac
Adam <adam.alrasi@outlook.com>
2023-10-14 13:27:36 +0100

    new kata

---
+++ 5 Kyus/Moving-Zeros-To-The-End.js
@@ -0,0 +1,19 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+var moveZeros = function (arr) {
+  var result = [];
+  var zeros = [];
+
+  for (var i in arr) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i]);
+    } else if (arr[i] !== 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result.concat(zeros);
+};
