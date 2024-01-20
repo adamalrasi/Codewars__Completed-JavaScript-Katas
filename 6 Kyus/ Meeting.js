33c8d17f149638a06d75e0b2b04b64343d120d7f
Adam Alrasi <adam.alrasi1@gmail.com>
2024-01-20 23:31:59 +0000

    Meeting

---
+++ 6 Kyus/ Meeting.js
@@ -0,0 +1,20 @@
+/*
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED
+*/
+
+function meeting(s) {
+    return s.toUpperCase().split(";")
+      .map(char => char.split(":").reverse())
+      .sort().reduce((acc, name) => acc += `(${name[0]}, ${name[1]})`, "")
+  }
