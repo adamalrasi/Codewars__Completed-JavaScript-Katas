fb34bdfaeb1b0e199a0e47ed12755c2cd3e03756
Adam Alrasi <adam.alrasi1@gmail.com>
2024-02-16 21:42:38 +0000

    The Hashtag Generator

---
+++ 5 Kyus/The Hashtag Generator.js
@@ -0,0 +1,20 @@
+/*
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+function generateHashtag(str){
+    if(str.trim() === "") return false;
+    const strCamelCase = '#'+ str.split(" ").map(char => char.charAt(0).toUpperCase() + char.slice(1)).join("");
+    return strCamelCase.length > 140 ? false : strCamelCase;
+  }
